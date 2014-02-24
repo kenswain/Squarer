c05bbcaa86daaba0407be20844217757bf4f9039
Ken Swain <ken@kenswain.com>
2014-02-24 03:26:56 -0600

    Initial commit of the Squarer problem form BNR book

---
+++ Squarer/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  Squarer
+//
+//  Created by Ken Swain on 2/24/14.
+//  Copyright (c) 2014 Ken Swain. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    int a = 5;
+    int b = a*a;
+    printf("\"%d\" squared is \"%d\".\n", a ,b);
+    return 0;
+    
+}
+
